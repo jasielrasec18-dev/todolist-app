ba3441baae5b1dd7a8a843bfaaca9da9ca82eb54
Jasiel Rasec <jasielrasec18@gmail.com>
2026-05-19 19:23:57 -0300

    feat: projeto completo todoApp

---
+++ src/contexts/theme.ts
@@ -0,0 +1,33 @@
+import IconSun from '/images/icon-sun.svg';
+import IconMoon from '/images/icon-moon.svg';
+
+export const themeConfig = {
+    light: {
+        name: 'light',
+        layout: {
+            heroClass: 'theme-light',
+            backgroundColor: 'bg-neutral-very-light-grayish-blue',
+            textColor: 'text-neutral-light-grayish-blue',
+        },
+        todo: {
+            backgroundColor: 'bg-neutral-very-light-gray',
+            borderColor: "border-neutral-very-light-grayish-blue",
+            textColor: 'text-neutral-very-dark-grayish-blue',
+        },
+        icon: IconMoon
+    },
+    dark: {
+        name: 'dark',
+        layout: {
+            heroClass: 'theme-dark',
+            backgroundColor: 'bg-neutral-very-dark-blue',
+            textColor: 'text-neutral-very-dark-grayish-blue',
+        },
+        todo: {
+            backgroundColor: 'bg-neutral-very-dark-desaturated-blue',
+            borderColor: "border-neutral-very-dark-grayish-blue",
+            textColor: 'text-neutral-light-grayish-blue',
+        },
+        icon: IconSun
+    },
+};        
